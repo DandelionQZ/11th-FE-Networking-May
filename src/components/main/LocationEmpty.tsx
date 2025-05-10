6a118a7b474a22e97606dd041bf2542708684a2d
ion <delion.dev@gmail.com>
2025-05-10 11:15:38 +0900

    [refactor#1] typescript에서 css 파일로 스타일 지정 변경

--- src/components/main/LocationEmpty.tsx
+++ src/components/main/LocationEmpty.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import dayClouds from '../../assets/dayClouds.svg';
+import './NoLocation.css';
 
 const LocationEmpty: React.FC = () => {
   return (
-    <div className='flex flex-col items-center gap-[24px]'>
-      <img
-        className='flex w-[320px] h-[320px] justify-center items-center aspect-square'
-        src={dayClouds}
-        alt='dayClouds'
-      />
-      <div className='text-black font-["Pretnedard"] font-4xl font-bold leading-normal'>
-        아직 선택된 위치가 없습니다!
-      </div>
+    <div className='no-location-container'>
+      <img className='no-location-image' src={dayClouds} alt='dayClouds' />
+      <div className='no-location-text'>아직 선택된 위치가 없습니다!</div>
     </div>
   );
 };
