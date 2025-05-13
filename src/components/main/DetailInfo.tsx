f8640c9be2ca7c664cc2299e485d3fe37789bdcc
ion <delion.dev@gmail.com>
2025-05-13 15:42:53 +0900

    [feat/#3] DetailInfo 컴포넌트 수정

--- src/components/main/DetailInfo.tsx
+++ src/components/main/DetailInfo.tsx
@@ -4,13 +4,17 @@ import './DetailInfo.css';
 interface DetailInfoProps {
   type: string;
   value: string;
+  bg: string;
+  txt: string;
 }
 
-const DetailInfo: React.FC<DetailInfoProps> = ({ type, value }) => {
+const DetailInfo: React.FC<DetailInfoProps> = ({ type, value, bg, txt }) => {
   return (
-    <div className='detail-info'>
-      <div className='tdetail-type'>{type}</div>
-      <div className='detail-value'>{value}</div>
+    <div className='detail-info' style={{ backgroundColor: bg }}>
+      <div className='detail-type'>{type}</div>
+      <div className='detail-value' style={{ color: txt }}>
+        {value}
+      </div>
     </div>
   );
 };
