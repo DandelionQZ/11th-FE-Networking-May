8dd5caff1dd678206d0f716b222006602b0df506
ion <delion.dev@gmail.com>
2025-05-15 01:01:37 +0900

    [feat/#18] 상태변수 값에 따른 MainPage 내부 컴포넌트 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MainPage.css';
 import MainWeather from '../components/main/MainWeather';
 import HourlyWeather from '../components/hourly/HourlyWeather';
 import WeeklyWeather from '../components/weekly/WeeklyWeather';
+import LocationEmpty from '../components/main/LocationEmpty';
 
 const MainPage: React.FC = () => {
-  return (
-    <div className='main-page'>
-      <MainWeather />
-      <HourlyWeather />
-      <WeeklyWeather />
-    </div>
-  );
+  const [isSelected] = useState<boolean>(true);
+
+  const display = () => {
+    if (isSelected) {
+      return (
+        <>
+          <MainWeather />
+          <HourlyWeather />
+          <WeeklyWeather />
+        </>
+      );
+    }
+    return <LocationEmpty />;
+  };
+
+  return <div className='main-page'>{display()}</div>;
 };
 
 export default MainPage;
