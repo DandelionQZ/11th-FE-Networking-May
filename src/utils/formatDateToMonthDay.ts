51a70f00b067a2b5c77e250577c64adfb1fb8e09
ion <delion.dev@gmail.com>
2025-05-14 12:32:55 +0900

    [feat/#9] formatDateToMonthDay 함수 작성 및 적용

---
+++ src/utils/formatDateToMonthDay.ts
@@ -0,0 +1,8 @@
+export const formatDateToMonthDay = (yyyymmdd: string): string => {
+  if (yyyymmdd.length !== 8) return yyyymmdd;
+
+  const month = parseInt(yyyymmdd.slice(4, 6), 10);
+  const day = parseInt(yyyymmdd.slice(6, 8), 10);
+
+  return `${month}.${day}`;
+};
