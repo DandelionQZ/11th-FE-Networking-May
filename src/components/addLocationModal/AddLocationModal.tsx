4fb7969ccfa2cf8a240caf0dcc7cf1137110fa44
ion <delion.dev@gmail.com>
2025-05-20 23:10:34 +0900

    [fix/#32] 위치추가 모달창 스타일 수정

--- src/components/addLocationModal/AddLocationModal.tsx
+++ src/components/addLocationModal/AddLocationModal.tsx
@@ -84,6 +84,7 @@ function AddLocationModal({ onClose, onSelectPlace }: AddLocationModalProps) {
 
         <div className='addlo-list-container'>
           {places.map((place) => (
+            // todo : 밑의 장소이름과 주소 담는 div 태그를 컴포넌트화 하기
             <div
               key={place.id}
               className={`addlo-list-item ${
