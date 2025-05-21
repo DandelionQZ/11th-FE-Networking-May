83a7f834c5841a53b0bf8acf224b67a3aeaf7dbd
ion <delion.dev@gmail.com>
2025-05-21 14:51:05 +0900

    [feat/#35] 날씨 데이터를 tanstack query로 관리
    weatherApi.ts에서 작성한 getTodayWeather mock Api 함수를 ContentPage.tsx에서 tanstack query로 관리하도록 했습니다.

--- src/pages/ContentPage.tsx
+++ src/pages/ContentPage.tsx
@@ -4,8 +4,26 @@ import MainWeather from '../components/main/MainWeather';
 import HourlyWeather from '../components/hourly/HourlyWeather';
 import WeeklyWeather from '../components/weekly/WeeklyWeather';
 import LocationEmpty from '../components/main/LocationEmpty';
+import { getTodayWeather } from '../api/weatherApi';
+import { useQuery } from '@tanstack/react-query';
 
 const ContentPage: React.FC = () => {
+  const lat = 100;
+  const lon = 999;
+
+  const { data, error, isSuccess } = useQuery({
+    queryKey: ['todayWeather'],
+    queryFn: () => getTodayWeather(lat, lon),
+  });
+
+  if (isSuccess) {
+    console.log('tanstack query todayWeather: ', data);
+  }
+
+  if (error) {
+    console.log('error: ', error);
+  }
+
   const [isSelected] = useState<boolean>(true);
 
   const display = () => {
