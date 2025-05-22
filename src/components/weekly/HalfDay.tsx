957af08613376bab53d4953a479bdd4083d1f325
ion <delion.dev@gmail.com>
2025-05-23 05:32:07 +0900

    [feat/#43] 주간 날씨 데이터 연결

--- src/components/weekly/HalfDay.tsx
+++ src/components/weekly/HalfDay.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import DayRain from '../../assets/DayRain.svg';
 import './HalfDay.css';
 
 interface HalfDayProps {
-  rain: number;
+  // rain: number;
   temp: number;
   m: string;
   c: string;
 }
 
-const HalfDay: React.FC<HalfDayProps> = ({ rain, temp, m, c }) => {
+const HalfDay: React.FC<HalfDayProps> = ({ /*rain,*/ temp, m, c }) => {
   return (
     <div className='half-day'>
-      <img src={DayRain} alt='DayRain' />
-      <div className='half-day-rain'>{rain}%</div>
+      {/* <div className='half-day-rain'>{rain}%</div> */}
       <div className='half-day-meridiem'>{m}</div>
       <div className='half-day-temp' style={{ color: `${c}` }}>
         {temp}º
