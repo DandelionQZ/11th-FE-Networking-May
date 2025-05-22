b109780d81c94582fea7d2186c081b028fc1e5ec
ion <delion.dev@gmail.com>
2025-05-22 10:38:17 +0900

    [feat/#34] 성공/실패 응답 처리 로직 작성

--- src/components/sign/SignForm.tsx
+++ src/components/sign/SignForm.tsx
@@ -4,22 +4,34 @@ import SignInput from './SignInput';
 import { useUserStore } from '../../store/userStore';
 import { useShallow } from 'zustand/shallow';
 import { postSignup } from '../../apis/user';
+import { useNavigate } from 'react-router-dom';
 
 interface SignFormProps {
   buttonText: string;
 }
 
 const SignForm: React.FC<SignFormProps> = ({ buttonText }) => {
-  const { email, password } = useUserStore(
+  const navigate = useNavigate();
+
+  const { email, password, clear } = useUserStore(
     useShallow((state) => ({
       email: state.email,
       password: state.password,
+      clear: state.clear,
     }))
   );
 
   const onHandleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    postSignup(email, password);
+    await postSignup(email, password)
+      .then(() => {
+        clear();
+        navigate('/login');
+      })
+      .catch((err) => {
+        alert('회원가입 실패');
+        console.log('회원가입 실패 err:', err);
+      });
   };
 
   return (
