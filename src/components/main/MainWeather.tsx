5990b269119f831b649cdfca1dbb3096eac50506
ion <delion.dev@gmail.com>
2025-05-23 05:51:08 +0900

    [feat/#47] 데이터 값 연결

--- src/components/main/MainWeather.tsx
+++ src/components/main/MainWeather.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ContentTitle from '../ContentTitle';
-import NightClouds from '../../assets/NightClouds.svg';
 import './MainWeather.css';
 import DetailInfo from './DetailInfo';
 import { useWeatherStore } from '../../store/weatherStore';
 import { useShallow } from 'zustand/shallow';
+import { getWeatherIcon } from '../../utils/exchangeIcon';
 
 const MainWeather: React.FC = () => {
   const { current } = useWeatherStore(
@@ -15,25 +15,28 @@ const MainWeather: React.FC = () => {
 
   return (
     <div className='weather-main-container'>
-      <ContentTitle text='4월 26일 롯데월드 날씨 현황' />
+      <ContentTitle text={current.datetime + ' 4월 26일 롯데월드 날씨 현황'} />
       {/* 날씨 현황 */}
       <div className='weather-main-current'>
         <div className='current-1st'>
-          <img src={NightClouds} alt='NightClouds' />
-          <div>12.2º</div>
+          <img
+            src={getWeatherIcon(current.weather.icon)}
+            alt='current.weather.icon'
+          />
+          <div>{current.temperature}º</div>
         </div>
         <div className='current-2nd'>야간 / 흐림</div>
         <div className='current-3rd'>
           <div>
-            체감 <span>9.0º</span>
+            체감 <span>{current.feelsLike}º</span>
           </div>
           <div className='sep'>●</div>
           <div>
-            습도 <span>48%</span>
+            습도 <span>{current.humidity}%</span>
           </div>
           <div className='sep'>●</div>
           <div>
-            남동풍 <span>0.4m/s</span>
+            {current.windDirection} <span>{current.windSpeed}m/s</span>
           </div>
         </div>
       </div>
@@ -42,8 +45,18 @@ const MainWeather: React.FC = () => {
       <div className='weather-main-detail'>
         <DetailInfo type='미세먼지' value='좋음' bg='#cce8ff' txt='#32a1ff' />
         <DetailInfo type='초미세먼지' value='보통' bg='#ceffcc' txt='#32ff35' />
-        <DetailInfo type='자외선' value='위험' bg='#ffcccc' txt='#ff3232' />
-        <DetailInfo type='일출' value='05:44' bg='#f6ffcc' txt='#ffc532' />
+        <DetailInfo
+          type='자외선'
+          value={current.uvIndex}
+          bg='#ffcccc'
+          txt='#ff3232'
+        />
+        <DetailInfo
+          type='일출'
+          value={current.sunrise}
+          bg='#f6ffcc'
+          txt='#ffc532'
+        />
       </div>
     </div>
   );
