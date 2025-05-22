c8b2420b0027612f3a64aeb788057568534df184
ion <delion.dev@gmail.com>
2025-05-23 06:00:03 +0900

    [feat/#47] 주간 또는 야간 반환 함수 getDayOrNight 작성

---
+++ src/utils/formatString.ts
@@ -0,0 +1,17 @@
+export const getDayOrNight = (icon: string): string => {
+  if (isNight(icon)) {
+    return '야간';
+  } else if (isDay(icon)) {
+    return '주간';
+  } else {
+    return '알 수 없음';
+  }
+};
+
+export const isNight = (code: string): boolean => {
+  return code.endsWith('n');
+};
+
+export const isDay = (code: string): boolean => {
+  return code.endsWith('d');
+};
