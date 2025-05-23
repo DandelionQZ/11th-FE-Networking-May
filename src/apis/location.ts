7e261abb89b7d213517bf096e86211072018d097
ion <delion.dev@gmail.com>
2025-05-23 12:43:22 +0900

    [feat/#49] 위치 추가 api 작성

--- src/apis/location.ts
+++ src/apis/location.ts
@@ -6,3 +6,19 @@ export const getLocations = async () => {
   const res = await authAxios.get(ENDPOINT.LOCATION_INFO);
   return res.data.data;
 };
+
+// 위치 추가 api
+export const postLocations = async (
+  locationName: string,
+  latitude: number,
+  longitude: number
+) => {
+  console.log('받은거 :::', locationName, latitude, longitude);
+
+  const res = await authAxios.post(ENDPOINT.LOCATION_INFO, {
+    locationName,
+    latitude,
+    longitude,
+  });
+  return res;
+};
